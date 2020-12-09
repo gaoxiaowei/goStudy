97770f278c6a636dabd22f9adceb70f7c0447e81
gaoxiaowei <gaoxiaowei@360.cn>
2020-12-09 16:46:54 +0800

    issue demo

--- helloworld/GithubIssue.go
+++ helloworld/GithubIssue.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	github2 "helloworld/github"
+	"fmt"
+	"helloworld/github"
+	"log"
 	"os"
 )
 
-func main()  {
-	github2.SearchIssues(os.Args[1:])
+func main(){
+	result, error := github.SearchIssues(os.Args[1:])
+	if error !=nil{
+		log.Fatal(error)
+	}
+	fmt.Printf("%d issues:\n",result.TotalCount)
+	for _,item :=range result.Items{
+		fmt.Printf("#%-5d %0.9s %.55s\n",item.Number,item.User.Login,item.Title)
+
+	}
 
 }
